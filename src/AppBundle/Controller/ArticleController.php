6dd518c6136260f465b37f01837fce5340cf61f6
dydy <dylanfoster.esgi@gmail.com>
2017-11-02 08:45:54 +0100

    submit btn on create view

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -12,13 +12,24 @@ use Symfony\Component\HttpFoundation\Request;
 class ArticleController extends Controller
 {
     /**
-     * @Route("/article/", name="article")
+     * @Route("/article/", name="admin_article_index")
      * @Method({"GET"})
      */
     public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('article/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        // Un repo est ce qui est appelé quand on veut taper sur une entité en passant par un controller. Doctrine les génère automatiquement. 
+        // findBy: 1st param groupe de conditions sur les colonnes,
+        // findBy: 2e param order & grouper
+        // Il y a aussi find($id), findAll, findById, findBySlug($slug || $id), findOneBy (retourne un seul élément)
+        // les finds retournent un objet/collection ou false 
+        $articles = $em->getRepository('AppBundle:Article')->findAll();
+        // return $this->render('article/index.html.twig');
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles
+        ]);
     }
 
     /**
